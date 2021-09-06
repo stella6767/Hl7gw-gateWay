61856308aad6e89eba826d32c4db9b9562dd2e80
강민규 <alsrb9434@gmail.com>
2021-09-06 11:28:46 +0900

    ftp make directory

--- src/main/java/net/lunalabs/hl7gw/service/FTPService.java
+++ src/main/java/net/lunalabs/hl7gw/service/FTPService.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 import org.apache.commons.net.PrintCommandListener;
 import org.apache.commons.net.ftp.FTP;
@@ -59,17 +60,34 @@ public class FTPService {
 		 "1234"); //localhost, 172.16.81.180
 
 		//ftpUploader.getFtpBean();"C:\\kangminkyu\\CsFTPSendText.txt"
+		 
+		String pattern = Pattern.quote(System.getProperty("file.separator"));
+
 		 		
 		logger.debug("filePath: " + filePath);
 		File fileTest = new File(filePath);
 		String fileName = fileTest.getName();
 		logger.debug("simpleFileName: " + fileName);
 		
-		ftpUploader.CheckAndMakeDirectory("/test");
+		logger.debug(fileTest.getAbsolutePath());
+		logger.debug(fileTest.getParent());
+		logger.debug(fileTest.getParentFile().toString());
+		logger.debug(fileTest.getCanonicalPath());
+		
+		String[] parentStrings =fileTest.getParent().split(pattern);
+		
+		logger.debug(parentStrings.toString());
+		
+		logger.debug(parentStrings[parentStrings.length -2]); //부모 폴더의 부모폴더이름을 뽑아낸다.
+		
+		String folderName = parentStrings[parentStrings.length -2];
+		
+		
+		ftpUploader.CheckAndMakeDirectory(File.separator + folderName);
 		
 		logger.debug("FTP SEND START");
 		//ftpUploader.uploadFile(filename, "CsFTPSendText.txt", "C:" + File.separator + "kangminkyu" +  File.separator  + "aaaaaaa" + File.separator);
-		ftpUploader.uploadFile(filePath, fileName, File.separator+"test" + File.separator); //파일 전송시 공백있는 이름의 파일을 전송하면 안 됨, 위에서 이미 공백을 다 제거했기 때문.
+		ftpUploader.uploadFile(filePath, fileName, File.separator+ folderName + File.separator); //파일 전송시 공백있는 이름의 파일을 전송하면 안 됨, 위에서 이미 공백을 다 제거했기 때문.
 		ftpUploader.disconnect();
 		logger.debug("FTP SEND DONE");
 		
