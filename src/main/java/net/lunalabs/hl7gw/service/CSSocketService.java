4c65d2125eeede4aad788298196367e27f342af5
강민규 <alsrb9434@gmail.com>
2021-10-12 08:55:18 +0900

    buffer size

--- src/main/java/net/lunalabs/hl7gw/service/CSSocketService.java
+++ src/main/java/net/lunalabs/hl7gw/service/CSSocketService.java
@@ -78,7 +78,7 @@ public class CSSocketService {
 
 //		while (isRunning && schn.isConnected()) {
 			
-			ByteBuffer readBuf = ByteBuffer.allocate(100); // 버퍼 메모리 공간확보
+			ByteBuffer readBuf = ByteBuffer.allocate(300); // 버퍼 메모리 공간확보
 			int bytesRead = schn.read(readBuf);
 			
 			String result = "";
@@ -115,119 +115,121 @@ public class CSSocketService {
              }
 			
 		
-			
+			logger.debug("소켓 닫기");
+			schn.close(); // 소켓 닫기
+
 
 //		}
 	}
 	
 	
 	
-	public void readSocketData(SocketChannel schn) throws IOException {
-
-		// SocketChannel schn = globalVar.globalSocket.get("schn");
-
-		// SocketChannel schn = csSocketStart();
-		concurrentConfig.globalSocketMap.put("cs", schn);
-
-		logger.debug("CS-socket 담김: " + schn);
-
-//        CMRespDto initResp = new CMRespDto<>(Opcode.INIT, globalVar.chargerid);
-//        writeSocket(initResp); //최초 자기 chargeId 전송
-
-		boolean isRunning = true; // 일단 추가, socketWork 중지할지 안 중지할지
-
-		while (isRunning && schn.isConnected()) {
-
-			try {
-				long lThId = Thread.currentThread().getId();
-				int byteCount = 0;
-				byte[] readByteArr;
-
-				ByteBuffer readBuf = ByteBuffer.allocate(10); // 버퍼 메모리 공간확보
-				// ByteBuffer readBuf = ByteBuffer.allocate(102400);
-
-				logger.debug("첫번째  while문");
-
-				// 무한 루프
-				String result = ""; // 요기서 초기화
-
-				while (byteCount >= 0) {
-
-					try {
-
-						byteCount = schn.read(readBuf); // 소켓채널에서 한번에 초과되는 버퍼사이즈의 데이터가 들어오면..
-
-						logger.debug("[gwEmulThread #100] TID[" + "] byteCount :  " + byteCount);
-						// logger.debug("isRunning why: " + isRunning);
-					} catch (Exception e) {
-						// e.printStackTrace();
-						logger.debug("갑자기 클라이언트 소켓이 닫혔을 시");
-						schn.close();
-						isRunning = false;
-						break;
-					}
-
-					int i = 0;
-
-					// 버퍼에 값이 있다면 계속 버퍼에서 값을 읽어 result 를 완성한다.
-					while (byteCount > 0) {
-
-						readBuf.flip(); // 입력된 데이터를 읽기 위해 read-mode로 바꿈, positon이 데이터의 시작인 0으로 이동
-						readByteArr = new byte[readBuf.remaining()]; // 현재 위치에서 limit까지 읽어드릴 수 있는 데이터의 개수를 리턴
-						readBuf.get(readByteArr); // 데이터 읽기
-
-						result = result + new String(readByteArr, Charset.forName("UTF-8"));
-
-						logger.debug("[gwEmulThread #200] TID[ " + lThId + "] socketRead Start[" + result
-								+ "], byteCount[" + byteCount + "], i[" + i + "]");
-						i++;
-
-						try {
-							byteCount = schn.read(readBuf);
-							logger.debug("[gwEmulThread #210] TID[" + result + "] byteCount :  " + byteCount);
-						} catch (Exception e) {
-							e.printStackTrace();
-							// break;
-						}
-
-						boolean bEtxEnd = true; // 아래 while문을 실행할지 안할지
-
-						// #ETX# 단위로 루프
-						while (!result.equals("") && bEtxEnd) {
-							logger.debug("완성 전 result: " + result);
-
-							Thread.sleep(1000);
-
-							if (readBuf.hasRemaining()) {
-
-								// - 계속 버퍼에서 읽어 append 함
-								logger.debug("position: " + readBuf.position());
-								logger.debug("result append: " + result);
-								readBuf.clear(); // position 초기화
-								logger.debug("position: " + readBuf.position());
-
-								break; // 이 블록에서 빠져나온다.
-							}
-
-//                         	parsingHl7toJson(result);
-//                            result = "";
-//                            bEtxEnd = false;
-//                            readBuf.clear();                            
-
-						}
-
-					} // #ETX# 단위로 루프
-				} // byteCount > 0
-
-				logger.debug("소켓 닫기");
-				schn.close(); // 소켓 닫기
-
-			} catch (Exception e) {
-				e.printStackTrace();
-				continue;
-			}
-		}
-	}
+//	public void readSocketData(SocketChannel schn) throws IOException {
+//
+//		// SocketChannel schn = globalVar.globalSocket.get("schn");
+//
+//		// SocketChannel schn = csSocketStart();
+//		concurrentConfig.globalSocketMap.put("cs", schn);
+//
+//		logger.debug("CS-socket 담김: " + schn);
+//
+////        CMRespDto initResp = new CMRespDto<>(Opcode.INIT, globalVar.chargerid);
+////        writeSocket(initResp); //최초 자기 chargeId 전송
+//
+//		boolean isRunning = true; // 일단 추가, socketWork 중지할지 안 중지할지
+//
+//		while (isRunning && schn.isConnected()) {
+//
+//			try {
+//				long lThId = Thread.currentThread().getId();
+//				int byteCount = 0;
+//				byte[] readByteArr;
+//
+//				ByteBuffer readBuf = ByteBuffer.allocate(300); // 버퍼 메모리 공간확보
+//				// ByteBuffer readBuf = ByteBuffer.allocate(102400);
+//
+//				logger.debug("첫번째  while문");
+//
+//				// 무한 루프
+//				String result = ""; // 요기서 초기화
+//
+//				while (byteCount >= 0) {
+//
+//					try {
+//
+//						byteCount = schn.read(readBuf); // 소켓채널에서 한번에 초과되는 버퍼사이즈의 데이터가 들어오면..
+//
+//						logger.debug("[gwEmulThread #100] TID[" + "] byteCount :  " + byteCount);
+//						// logger.debug("isRunning why: " + isRunning);
+//					} catch (Exception e) {
+//						// e.printStackTrace();
+//						logger.debug("갑자기 클라이언트 소켓이 닫혔을 시");
+//						schn.close();
+//						isRunning = false;
+//						break;
+//					}
+//
+//					int i = 0;
+//
+//					// 버퍼에 값이 있다면 계속 버퍼에서 값을 읽어 result 를 완성한다.
+//					while (byteCount > 0) {
+//
+//						readBuf.flip(); // 입력된 데이터를 읽기 위해 read-mode로 바꿈, positon이 데이터의 시작인 0으로 이동
+//						readByteArr = new byte[readBuf.remaining()]; // 현재 위치에서 limit까지 읽어드릴 수 있는 데이터의 개수를 리턴
+//						readBuf.get(readByteArr); // 데이터 읽기
+//
+//						result = result + new String(readByteArr, Charset.forName("UTF-8"));
+//
+//						logger.debug("[gwEmulThread #200] TID[ " + lThId + "] socketRead Start[" + result
+//								+ "], byteCount[" + byteCount + "], i[" + i + "]");
+//						i++;
+//
+//						try {
+//							byteCount = schn.read(readBuf);
+//							logger.debug("[gwEmulThread #210] TID[" + result + "] byteCount :  " + byteCount);
+//						} catch (Exception e) {
+//							e.printStackTrace();
+//							// break;
+//						}
+//
+//						boolean bEtxEnd = true; // 아래 while문을 실행할지 안할지
+//
+//						// #ETX# 단위로 루프
+//						while (!result.equals("") && bEtxEnd) {
+//							logger.debug("완성 전 result: " + result);
+//
+//							Thread.sleep(1000);
+//
+//							if (readBuf.hasRemaining()) {
+//
+//								// - 계속 버퍼에서 읽어 append 함
+//								logger.debug("position: " + readBuf.position());
+//								logger.debug("result append: " + result);
+//								readBuf.clear(); // position 초기화
+//								logger.debug("position: " + readBuf.position());
+//
+//								break; // 이 블록에서 빠져나온다.
+//							}
+//
+////                         	parsingHl7toJson(result);
+////                            result = "";
+////                            bEtxEnd = false;
+////                            readBuf.clear();                            
+//
+//						}
+//
+//					} // #ETX# 단위로 루프
+//				} // byteCount > 0
+//
+//				logger.debug("소켓 닫기");
+//				schn.close(); // 소켓 닫기
+//
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//				continue;
+//			}
+//		}
+//	}
 
 	public void writeSocket(String Hl7parsingData) throws JsonProcessingException {
 
